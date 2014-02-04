2ebef57a662708b51adfed3812b9e3f422894306
Alvin Norin <alvin.norin@gmail.com>
2014-02-04 20:24:58 +0100

    debugging

--- Snake/p.py
+++ Snake/p.py
@@ -4,13 +4,13 @@ import os
 
 def push() :
 	comment = None
-	os.system("cd && cd python && dir && git add Snake")
+	os.system("cd && cd python && git add Snake")
 	comment = raw_input("Comment: ")
 	os.system("git commit -m \""+comment+"\"")
 	os.system("git push origin master")
 
 while True :
-	usrInput = raw_input("1) Push\n2) Pull\n")
+	usrInput = raw_input("1) Push\n2) Pull\n(")
 	if str(usrInput) == '1' :
 		push()
 	elif usrInput == 2 :
