0f53a56364521f87fc205cc0bc478de438104308
Alvin Norin <alvin.norin@gmail.com>
2014-02-04 20:09:03 +0100

    Hej

--- Snake/p.py
+++ Snake/p.py
@@ -3,13 +3,18 @@
 import os
 
 count = 0
-usrInput = [99]
+usrInput = ['p.py', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+comment = None
 
 while usrInput[count] != '' :
 	usrInput[count] = raw_input("Add: ")
-	count+=1
-max = count
+	count += 1
+Max = count
 count = 0
-while count <= max
+while count <= Max :
+	os.system("git add "+usrInput[count])
+	count += 1
+comment = raw_input("Comment: ")
+os.system("git commit -m \""+comment+"\"")
+os.system("git push origin master")
 
-os.system("")
